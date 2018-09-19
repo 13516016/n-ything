19f9a875ad5a866bcd0dd36a9fc04b140788c9c8
azkanab <azkanabilah@gmail.com>
2018-09-19 14:07:04 +0700

    Parser has been fixed

--- util/parser.py
+++ util/parser.py
@@ -1,38 +1,32 @@
-import sys
-sys.path.append('../')
-
 from model import *
 
-filetest = open("test1.txt", "r")
+def parse(filename) :
+	filetest = open(filename, "r")
 
-arrcomponent = []
+	arrcomponent = []
 
-for line in filetest :
-	component = line.split(" ")
-	color = component[0].lower()
-	types = component[1].lower()
-	print(component[0])
-	print(component[1])
-	print(component[2])
-	n = int(component[2])
-	if types == "knight" :
-		knight = Knight(int(0), int(0), color)
-		for i in range (0, n) :
-			arrcomponent.append(knight)
-	elif types == "bishop" :
-		bishop = Bishop(int(0), int(0), color)
-		for i in range (0, n) :
-			arrcomponent.append(bishop)
-	elif types == "rook" :
-		rook = Rook(int(0), int(0), color)
-		for i in range (0, n) :
-			arrcomponent.append(rook)
-	elif types == "queen" :
-		queen = Queen(int(0), int(0), color)
-		for i in range (0, n) :
-			arrcomponent.append(queen)
+	for line in filetest :
+		component = line.split(" ")
+		color = component[0].lower()
+		types = component[1].lower()
+		n = int(component[2])
+		if types == "knight" :
+			knight = Knight(0, 0, color)
+			for i in range (0, n) :
+				arrcomponent.append(knight)
+		elif types == "bishop" :
+			bishop = Bishop(0, 0, color)
+			for i in range (0, n) :
+				arrcomponent.append(bishop)
+		elif types == "rook" :
+			rook = Rook(0, 0, color)
+			for i in range (0, n) :
+				arrcomponent.append(rook)
+		elif types == "queen" :
+			queen = Queen(0, 0, color)
+			for i in range (0, n) :
+				arrcomponent.append(queen)
 
-print()
+	filetest.close()
 
-for each in arrcomponent :
-	print(each.x,'',each.y,'',each.color)
+	return arrcomponent

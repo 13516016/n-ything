5a95f66c4adcb383319f776624aa50efa7943e0c
azkanab <azkanabilah@gmail.com>
2018-09-20 21:56:03 +0700

    Fitness has been added

--- nything/fitness.py
+++ nything/fitness.py
@@ -3,5 +3,10 @@ sys.path.append('../')
 
 from model import *
 
-def fitness() :
-	return
+def fitness(chess_pieces) :
+	countenemy = 0
+	countally = 0
+	for each in chess_pieces :
+		countenemy += each.count_attacked_enemy(chess_pieces)
+		countally += each.count_attacked_ally(chess_pieces)
+	return(countally, countenemy)

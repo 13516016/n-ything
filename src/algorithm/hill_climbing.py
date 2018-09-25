5eca9510538fb959872a5dc3dd8c2f784398762b
Adylan Roaffa <adylanrff@gmail.com>
2018-09-25 12:34:47 +0700

    fix indentation

--- src/algorithm/hill_climbing.py
+++ src/algorithm/hill_climbing.py
@@ -6,7 +6,7 @@ def hill_climbing(chess_pieces, max_iteration):
     #inisiasi kondisi
     i = 0
     while (i < max_iteration):
-    	generate_move(chess_pieces)
-    	i = i +1
+        generate_move(chess_pieces)
+        i = i +1
 
     return chess_pieces
